ba4a94015463cc7ced2e2e3b67a41fe4deefffc8
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2023-12-17 18:13:33 +0530

    small bug fixed

--- src/contexts/TodoContexts.js
+++ src/contexts/TodoContexts.js
@@ -12,8 +12,8 @@ export const TodoContexts = createContext({
             completed: false,
         }
     ],
-    addTodo: (todo) = {},
-    updateTodo: (id, todo) => { },
+    addTodo: (todo) => { },
+    updateTodo: (id,todo) => { },
     deleteTodo: (id) => { },
     toggleComplete: (id) => { }
 });
