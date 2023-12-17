cc86a611d236a77543e8a7dd3a5ea0c72994a474
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2023-12-17 18:10:13 +0530

    All Components mounted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { TodoProvider } from "./contexts/TodoContexts";
+import TodoForm from './components/TodoForm';
+import TodoItem from './components/TodoItem';
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
-    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+    setTodos((prev) => [{id: Date.now(), ...todo }, ...prev]);
   };
 
   const updateTodo = (id, todo) => {
@@ -37,6 +39,10 @@ function App() {
     }
   } , [])
 
+  useEffect(() => {
+    localStorage.setItem("todos" , JSON.stringify(todos))
+  } , [todos])
+
 
   return (
     <TodoProvider
@@ -47,9 +53,19 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} 
+              className='w-full'
+              >
+                <TodoItem todo={todo} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
